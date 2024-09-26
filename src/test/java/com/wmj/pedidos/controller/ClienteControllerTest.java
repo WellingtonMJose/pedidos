594e38f5004cc1b5f21aee5a19066336be797ddb
Wellington <wellingtonmoacirjose@gmail.com>
2024-09-26 10:10:40 -0300

    Ajuste no properties e test

--- src/test/java/com/wmj/pedidos/controller/ClienteControllerTest.java
+++ src/test/java/com/wmj/pedidos/controller/ClienteControllerTest.java
@@ -5,7 +5,6 @@ import com.wmj.pedidos.service.ClienteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
